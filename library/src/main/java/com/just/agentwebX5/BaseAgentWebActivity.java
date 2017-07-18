53f3c756cddcf973b3ea8dfb68adba01c39b5eae
Justson <xiaozhongcen@gmail.com>
2017-07-18 23:44:25 +0800

    add base

---
+++ library/src/main/java/com/just/agentwebX5/BaseAgentWebActivity.java
@@ -0,0 +1,17 @@
+package com.just.agentwebX5;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by cenxiaozhong on 2017/7/18.
+ */
+
+public class BaseAgentWebActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
