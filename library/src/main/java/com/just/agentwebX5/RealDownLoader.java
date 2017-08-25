fa03c0abd9c495d3f096b308f84b7bb27e9a4384
Justson <xiaozhongcen@gmail.com>
2017-08-25 22:07:59 +0800

    #

--- library/src/main/java/com/just/agentwebX5/RealDownLoader.java
+++ library/src/main/java/com/just/agentwebX5/RealDownLoader.java
@@ -74,7 +74,7 @@ public class RealDownLoader extends AsyncTask<Void, Integer, Integer> implements
         super.onPreExecute();
 
         mObservable.addObserver(this);
-        buildNotify(new Intent(), mDownLoadTask.getId(),mDownLoadTask.getDownLoadMsgConfig().getPreLoading());
+        buildNotify(new Intent(), mDownLoadTask.getId(), mDownLoadTask.getDownLoadMsgConfig().getPreLoading());
     }
 
     private boolean checkDownLoaderCondition() {
@@ -154,7 +154,7 @@ public class RealDownLoader extends AsyncTask<Void, Integer, Integer> implements
 
         HttpURLConnection mHttpURLConnection = (HttpURLConnection) new URL(url).openConnection();
         mHttpURLConnection.setRequestProperty("Accept", "application/*");
-        mHttpURLConnection.setConnectTimeout(5000*2);
+        mHttpURLConnection.setConnectTimeout(5000 * 2);
         return mHttpURLConnection;
     }
 
@@ -173,17 +173,16 @@ public class RealDownLoader extends AsyncTask<Void, Integer, Integer> implements
                 if (!mNotity.hasDeleteContent())
                     mNotity.setDelecte(buildCancelContent(mDownLoadTask.getContext().getApplicationContext(), mDownLoadTask.getId()));
 
-                int mProgress=(int) ((tmp + loaded) / Float.valueOf(totals) * 100);
-                mNotity.setContentText(String.format(mDownLoadTask.getDownLoadMsgConfig().getLoading(),mProgress+"%"));
-                mNotity.setProgress(100,mProgress , false);
+                int mProgress = (int) ((tmp + loaded) / Float.valueOf(totals) * 100);
+                mNotity.setContentText(String.format(mDownLoadTask.getDownLoadMsgConfig().getLoading(), mProgress + "%"));
+                mNotity.setProgress(100, mProgress, false);
             }
 
-        }catch (UnknownFormatConversionException e){
+        } catch (UnknownFormatConversionException e) {
             e.printStackTrace();
         }
         long current = System.currentTimeMillis();
         used = current - begin;
-
 
 
     }
@@ -209,16 +208,16 @@ public class RealDownLoader extends AsyncTask<Void, Integer, Integer> implements
                     mNotity.cancel(mDownLoadTask.getId());
 
                 Intent intent = AgentWebUtils.getIntentCompat(mDownLoadTask.getContext(), mDownLoadTask.getFile());
-                // intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+                intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
                 PendingIntent rightPendIntent = PendingIntent.getActivity(mDownLoadTask.getContext(),
-                        mDownLoadTask.getId()<<4, intent, PendingIntent.FLAG_UPDATE_CURRENT);
+                        mDownLoadTask.getId() << 4, intent, PendingIntent.FLAG_UPDATE_CURRENT);
                 mNotity.setProgressFinish(mDownLoadTask.getDownLoadMsgConfig().getClickOpen(), rightPendIntent);
 
             }
 
-        }catch (Exception e){
+        } catch (Exception e) {
             e.printStackTrace();
-            LogUtils.i("Info","e:"+e.getMessage());
+            LogUtils.i("Info", "e:" + e.getMessage());
         }
 
 
@@ -227,7 +226,7 @@ public class RealDownLoader extends AsyncTask<Void, Integer, Integer> implements
     private void doCallback(Integer code) {
         DownLoadResultListener mDownLoadResultListener = null;
         LogUtils.i("Info", " doCallback  mDownLoadTask.getDownLoadResultListeners():" + mDownLoadTask.getDownLoadResultListener());
-        if ((mDownLoadResultListener = mDownLoadTask.getDownLoadResultListener())==null) {
+        if ((mDownLoadResultListener = mDownLoadTask.getDownLoadResultListener()) == null) {
             return;
         }
         if (code > 200) {
@@ -272,7 +271,7 @@ public class RealDownLoader extends AsyncTask<Void, Integer, Integer> implements
     private int doDownLoad(InputStream in, RandomAccessFile out) throws IOException {
 
         byte[] buffer = new byte[102400];
-        BufferedInputStream bis = new BufferedInputStream(in, 1024*10);
+        BufferedInputStream bis = new BufferedInputStream(in, 1024 * 10);
         try {
 
             out.seek(out.length());
@@ -282,7 +281,7 @@ public class RealDownLoader extends AsyncTask<Void, Integer, Integer> implements
 
 
             while (!atomic.get()) {
-                int n = bis.read(buffer, 0, 1024*10);
+                int n = bis.read(buffer, 0, 1024 * 10);
                 if (n == -1) {
                     break;
                 }
@@ -304,7 +303,7 @@ public class RealDownLoader extends AsyncTask<Void, Integer, Integer> implements
                 }
             }
 
-            LogUtils.i("Info", "atomic:" + atomic.get() );
+            LogUtils.i("Info", "atomic:" + atomic.get());
             if (atomic.get()) {
                 return DownLoadMsg.USER_CANCEL.CODE;
             }
@@ -428,7 +427,6 @@ public class RealDownLoader extends AsyncTask<Void, Integer, Integer> implements
 
 
     }
-
 
 
 }
