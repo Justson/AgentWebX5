16446a4a5d8cd6fe48f5d3241688fe3f5ed7059c
Justson <xiaozhongcen@gmail.com>
2017-11-30 13:43:51 +0800

    rename AgentWebJsInterfaceX5Compat.

--- library/src/main/java/com/just/agentwebX5/AgentWebJsInterfaceX5Compat.java
+++ library/src/main/java/com/just/agentwebX5/AgentWebJsInterfaceX5Compat.java
@@ -7,11 +7,11 @@ import android.webkit.JavascriptInterface;
  * Created by cenxiaozhong on 2017/5/24.
  */
 
-public class AgentWebJsInterfaceCompat implements AgentWebCompat ,FileUploadPop<IFileUploadChooser> {
+public class AgentWebJsInterfaceX5Compat implements AgentWebX5Compat,FileUploadPop<IFileUploadChooser> {
 
     private AgentWebX5 mAgentWebX5;
     private Activity mActivity;
-     AgentWebJsInterfaceCompat(AgentWebX5 agentWebX5, Activity activity){
+     AgentWebJsInterfaceX5Compat(AgentWebX5 agentWebX5, Activity activity){
         this.mAgentWebX5 = agentWebX5;
          this.mActivity=activity;
     }
