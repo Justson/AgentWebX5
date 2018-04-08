bc4aa67d84c65637e0871fbda63d72d80eef4ba7
Justson <xiaozhongcen@gmail.com>
2018-04-08 23:23:45 +0800

    #

--- library/src/main/java/com/just/agentwebX5/AgentWebX5Utils.java
+++ library/src/main/java/com/just/agentwebX5/AgentWebX5Utils.java
@@ -70,16 +70,6 @@ import static android.content.ContentValues.TAG;
 import static com.just.agentwebX5.AgentWebX5Config.AGENTWEB_FILE_PATH;
 import static com.just.agentwebX5.AgentWebX5Config.FILE_CACHE_PATH;
 
-/**
- * <b>@项目名：</b> agentweb<br>
- * <b>@包名：</b>com.just.library<br>
- * <b>@创建者：</b> cxz --  just<br>
- * <b>@创建时间：</b> &{DATE}<br>
- * <b>@公司：</b><br>
- * <b>@邮箱：</b> cenxiaozhong.qqcom@qq.com<br>
- * <b>@描述</b><br>
- * source CODE  https://github.com/Justson/AgentWebX5
- */
 
 public class AgentWebX5Utils {
 
