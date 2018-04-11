2ee1f7f211d6284fc511db3cbdd0a89504d8a420
Justson <xiaozhongcen@gmail.com>
2018-04-11 23:07:36 +0800

    #

--- library/src/main/java/com/just/agentwebX5/WebSecurityControllerImpl.java
+++ library/src/main/java/com/just/agentwebX5/WebSecurityControllerImpl.java
@@ -7,13 +7,6 @@ import com.tencent.smtt.sdk.WebView;
 
 
 /**
- * <b>@项目名：</b> <br>
- * <b>@包名：</b><br>
- * <b>@创建者：</b> cxz --  just<br>
- * <b>@创建时间：</b> &{DATE}<br>
- * <b>@公司：</b> <br>
- * <b>@邮箱：</b> cenxiaozhong.qqcom@qq.com<br>
- * <b>@描述</b>source code https://github.com/Justson/AgentWebX5<br>
  */
 
 public class WebSecurityControllerImpl implements WebSecurityController<WebSecurityCheckLogic> {
