8e10a750cdc4126788bcc359f875baba74c50641
Justson <xiaozhongcen@gmail.com>
2018-04-23 23:59:58 +0800

    #

--- library/src/main/java/com/just/agentwebX5/AgentWebX5Utils.java
+++ library/src/main/java/com/just/agentwebX5/AgentWebX5Utils.java
@@ -73,11 +73,6 @@ import static com.just.agentwebX5.AgentWebX5Config.FILE_CACHE_PATH;
 
 public class AgentWebX5Utils {
 
-    public static int px2dp(Context context, float pxValue) {
-
-        final float scale = context.getResources().getDisplayMetrics().density;
-        return (int) (pxValue / scale + 0.5f);
-    }
 
     public static int dp2px(Context context, float dipValue) {
 
